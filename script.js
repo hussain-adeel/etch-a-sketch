d6ed8531b672581ae4eb3da310704ba568b13779
Adeel Hussain <adeelh117@gmail.com>
2020-08-20 20:15:37 -0700

    Added sketch functionality + reset button + change size button

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
  {
     createRows(multiplier)
     createColumns(multiplier)
+    var unpaintedCells = document.querySelectorAll("#noPaint")
+    var paintedCells = document.querySelectorAll("#painted")  
+    
+    // Allows for painting of the grid
+    unpaintedCells.forEach(function(cell) 
+    {
+       cell.addEventListener('mouseover', function paint()
+       {
+          cell.id = 'painted'
+       })
+    })
+    
+    // Allows for use of reset button
+    resetButton.addEventListener('click', function clearGrid() 
+    {
+       unpaintedCells.forEach(function(cell)
+       {
+          cell.id = 'noPaint'
+       })
+    })
  }
  function createRows(numRows)
  {
@@ -19,16 +39,53 @@
          {
             let newCol = document.createElement('div')
             newCol.classList.add('col')
-            newCol.id = "no"
+            newCol.id = "noPaint"
             rows[i].appendChild(newCol)
          }
  }
-function paintCell() 
-{
-   let unPainted = document.getElementById("noPaint")
-   unPainted.style.backgroundColor = "blue"
-}
+ function resetGrid() 
+ {
+   const oldRows = document.querySelectorAll("div.row")
+   const oldCols = document.querySelectorAll("div.col")
+   oldCols.forEach(function(col){
+      col.remove()
+   })
+   oldRows.forEach(function(row){
+      row.remove()
+   })
+ }
+ function changeGridSize(multiplier) 
+ {
+   resetGrid()
+   createGrid(multiplier)
+ }
+//Constants that can be called before making grid
 const container = document.getElementById('container')
 const rows = document.getElementsByClassName('row')
-createGrid(16)
+const resetButton = document.getElementById('resetb')
+const sizeButton = document.getElementById('sizeb')
+
+// Inital Grid
+changeGridSize(16)
+
+
+// Allows for use of change grid size button
+sizeButton.addEventListener('click', function reset()
+{
+   let userInput = 16
+   let valid = false
+   while (!valid)
+   {
+      userInput = prompt("What would you like the new size to be? (1-100)")
+      if (userInput > 100 || userInput < 1 || isNaN(userInput))
+      {
+         continue;
+      }
+      valid = true;
+   }
+   changeGridSize(userInput)
+})
+
+
+
 
